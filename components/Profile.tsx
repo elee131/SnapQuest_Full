bfa4e2b2680c9bebfc3860fcf39878655165c368
Anica1004 <anicamok@gmail.com>
2024-05-24 21:14:36 -0700

    completed changing the profile picture

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,57 +1,110 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, SafeAreaView, Image, Animated, Easing } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, SafeAreaView, Image, TouchableOpacity, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { profile } from '@/assets/data/images';
 import { useUser } from '../context/UserContext';
-
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 const ProfileScreen = () => {
-  const { userUID, username, email, currStreak, longestStreak, profilePic, point, images  } = useUser();
+  const { username, currStreak, longestStreak, profilePic, point, images, setProfilePic } = useUser();
+  const [showPictures, setPictures] = useState(false);
+
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setProfilePic(result.assets[0].uri);
+    }
+  };
+
 
   return (
-    <SafeAreaView>
-      <ScrollView style={styles.container}>
+    <SafeAreaView style={styles.safeArea}>
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
         <View style={styles.profileContainer}>
-        {profilePic ? (
-          <Image source={{ uri: profilePic }} style={styles.profileImage} />
-        ) : (
-          <Image source={profile} style={styles.profileImage} />
-        )}
+          <Image source={{ uri: profilePic || profile }} style={styles.profileImage} />
+          <TouchableOpacity onPress={pickImage} style={styles.editIconContainer}>
+            <Image source={require('../assets/images/edit.png')} style={styles.editImage} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.profileText}>
           <Text style={styles.username}>{username}</Text>
           <Text style={styles.streakText}>
             Current Streak: {currStreak} days | Longest Streak: {longestStreak} days
           </Text>
-          <Text style={styles.streakText}>
-            Current Rewards: {point} Points
+          <Text style={styles.rewardsText}>
+            Current Rewards: 
+            <MaterialCommunityIcons name="crown-outline" size={24} color="gold" style={{ marginLeft: 3, marginRight: 3 }} />
+            {point} Points 
+            <MaterialCommunityIcons name="crown-outline" size={24} color="gold" style={{ marginLeft: 3 }} />
           </Text>
         </View>
-        <Text style={styles.header}>
-          Pictures you have taken:
-        </Text>
-        <View style={styles.imagesContainer}>
-          {images.map((image, index) => (
-            <View style={styles.picHolder} key={index}>
-              <Image source={{uri: image}} style={styles.image} />
+        
+        {showPictures ? (
+          <>
+            <TouchableOpacity onPress={() => setPictures(false)}>
+              <Image source={require('../assets/images/openImage.png')} style={styles.imageIcon} />
+              <Text style={styles.buttonfont}>Hide Pictures</Text>
+            </TouchableOpacity>
+
+            <View style={styles.imagesContainer}>
+              {images.map((image, index) => (
+                <View style={styles.picHolder} key={index}>
+                  <Image source={{ uri: image }} style={styles.image} />
+                </View>
+              ))}
             </View>
-          ))}
-        </View>
+          </>
+        ) : (
+          <TouchableOpacity onPress={() => setPictures(true)}>
+            <Image source={require('../assets/images/showImage.png')} style={styles.imageIcon} />
+            <Text style={styles.buttonfont}>Show Pictures</Text>
+          </TouchableOpacity>
+        )}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scrollViewContent: {
+    flexGrow: 1,
   },
   profileContainer: {
     alignItems: 'center',
-    marginTop: "5%", 
-    marginBottom: "15%",
+    marginTop: '10%',
+    marginBottom: '15%',
+    position: 'relative', 
   },
   profileImage: {
-    width: 120,
-    height: 120,
-    borderRadius: 60,
+    width: 130,
+    height: 130,
+    borderRadius: 65, 
+  },
+  editIconContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    marginRight: "25%", 
+  },
+  editImage: {
+    width: 30,
+    height: 30,
+  },
+  profileText: {
+    alignItems: 'center', 
   },
   username: {
     marginTop: 10,
@@ -63,17 +116,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: 'gray',
   },
-  header: {
-    fontSize: 25,
-    marginBottom: 16,
-    paddingHorizontal: 16,
-    fontFamily: "cute-font",
+  rewardsText: {
+    marginTop: 15,
+    fontSize: 20,
+    fontFamily: 'CelosiaGolden',
   },
   imagesContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
-    paddingHorizontal: 10, // Add padding to the sides of the images container
+    paddingHorizontal: 20,
   },
   picHolder: {
     width: 100,
@@ -84,24 +136,18 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
-    borderRadius: 10,
+    borderRadius: 5,
   },
-  textfont: {
-    padding: 5,
+  buttonfont: {
     fontSize: 12,
     fontWeight: 'bold',
+    textAlign: 'center',
   },
-  progressBarBackground: {
-    width: '100%',
-    height: 20,
-    backgroundColor: '#e0e0e0',
-    borderRadius: 10,
-    marginTop: 10, // Add margin to the top of the progress bar
-  },
-  progressBarFill: {
-    height: '100%',
-    backgroundColor: '#1A535C',
-    borderRadius: 10,
+  imageIcon: {
+    marginTop: 0,
+    width: 80,
+    height: 80,
+    alignSelf: 'center',
   },
 });
 
