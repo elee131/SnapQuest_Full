ac22787877ad91ff2cab1b18aa6b41cfec1dcb17
Anica1004 <anicamok@gmail.com>
2024-06-06 15:03:02 -0700

    clear email&password when user is back to login pg

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,95 +1,79 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from 'firebaseConfig';
 import { useUser } from '../context/UserContext';
+import { useFocusEffect } from '@react-navigation/native';
 
-
-
-const LoginScreen = ({navigation}: any) => {
+const LoginScreen = ({ navigation }: any) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { initUser } = useUser();
 
   const handleLogin = () => {
     signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    initUser(user.uid);
-    navigation.navigate('Profile'); 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
-    
+      .then((userCredential) => {
+        const user = userCredential.user;
+        initUser(user.uid);
+        navigation.navigate('Profile');
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+      });
   };
 
   const handleCreateAccount = () => {
-    navigation.navigate('CreateAccount'); 
+    navigation.navigate('CreateAccount');
   };
-return (
-    <View style =  {styles.container}>
-      <View style = {styles.secondContainer}>
 
-{/* <Image source={require("@/assets/images/logo.jpg")} style={styles.logoStyle} /> */}
+  useFocusEffect(
+    useCallback(() => {
+      setEmail('');
+      setPassword('');
+    }, [])
+  );
 
-    <Text style={styles.title}>Welcome Back!</Text>
-    <Text style={styles.subtitle}>Please log in to check your profile</Text>
- <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-<TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={setPassword}
-      />
-
-       <TouchableOpacity style={styles.button} onPress={handleLogin}>
-         <Text style={styles.buttonText}>Login</Text>
-      </TouchableOpacity>
-
-          <TouchableOpacity style={styles.createAccountButton} onPress={handleCreateAccount}>
-       <Text style={styles.createAccountText}>Create an Account</Text>
-      </TouchableOpacity>
- 
-
+  return (
+    <View style={styles.container}>
+      <View style={styles.secondContainer}>
+        <Text style={styles.title}>Welcome Back!</Text>
+        <Text style={styles.subtitle}>Please log in to check your profile</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          value={password}
+          onChangeText={setPassword}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleLogin}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.createAccountButton} onPress={handleCreateAccount}>
+          <Text style={styles.createAccountText}>Create an Account</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    </View>
-); 
-}
-
-
-
-
+  );
+};
 
 const styles = StyleSheet.create({
-
   container: {
-    flex: 1, // Ensure the container stretches to fill available space
+    flex: 1,
     backgroundColor: "#fff",
     justifyContent: 'center',
   },
   secondContainer: {
     alignItems: 'center',
-    marginRight: "10%", 
+    marginRight: "10%",
     marginLeft: "10%",
-  }, 
-  check: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  heading: {
-    fontSize: 24,
-    marginBottom: 20,
   },
   input: {
     width: '100%',
@@ -104,8 +88,7 @@ const styles = StyleSheet.create({
   button: {
     width: '100%',
     height: 40,
-    // backgroundColor: 'blue',
-    backgroundColor : "#568C69",
+    backgroundColor: "#568C69",
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 5,
@@ -118,8 +101,7 @@ const styles = StyleSheet.create({
     marginTop: 10,
     width: '100%',
     height: 40,
-    // backgroundColor: 'green',
-    backgroundColor : "#0C403B",
+    backgroundColor: "#0C403B",
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 5,
@@ -131,7 +113,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 28,
     fontWeight: 'bold',
-    color: "black", 
+    color: "black",
     marginBottom: 10,
     fontFamily: 'mon-b'
   },
@@ -140,15 +122,6 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     color: '#888',
     fontFamily: 'mon-sb'
-  },
-  color:{
-    backgroundColor: "#fff", 
-  },
-  logoStyle: {
-    width: 150, // Adjust the width as needed
-    height: 150, // Adjust the height as needed
-    marginBottom: 20, // Optional: add margin if needed
-    resizeMode: 'contain', // Ensure the image fits the container without stretching
   },
 });
 
