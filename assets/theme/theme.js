29cff429261a2ea0d70b9bd0b56876a69d02e0fe
Anica1004 <anicamok@gmail.com>
2024-06-07 20:07:17 -0700

    changed the dark mode color

--- assets/theme/theme.js
+++ assets/theme/theme.js
@@ -1,36 +1,18 @@
 const theme ={
     light:{
-        // theme: 'light', 
-        // backgroundHome: "#FFC0CB", 
-        // snapColor: "#1A535C", 
-        // timeColor: "#666666",
-        // streakColor: "#1A535C", 
-        // completeColor: '#FF6B6B',
-        
         content: "white",
         background: "#FFF8F0",
+        //"hsl(350, 100%, 88%)" is pretty though..
         color: "#2C241D",
-
 
         light: "#FFF8F0",
         midrange: "#F7CE5B", 
         dark: "#FFA62B",
-     
-
     }, 
     dark:{
-        // theme:'dark', 
-        // backgroundHome: "#005AB5",
-        // snapColor: "white", 
-        // timeColor: "white", 
-        // streakColor: "white", 
-        // completeColor: "white",
-
-        background: "#192734",
-        color: "#15292B",
-        content: "#888", 
-
-
+        background: "#161618",
+        color: "#888", // or #666666"
+        content: "#242526", 
 
         light: "#95BFA0", 
         midrange: "#568C69",
